12a161ee61cb406eee2e2b8e53dfa792a52377a8
Neelzee <nilsien2001@gmail.com>
2023-10-24 10:05:41 +0200

    finished getters for author

--- src/db/entities/author/mod.rs
+++ src/db/entities/author/mod.rs
@@ -1,4 +1,7 @@
+use rusqlite::{Connection, Statement};
 use serde::{Deserialize, Serialize};
+
+use crate::utils::constants::db::author::{AUTHOR_TABLE_NAME, AUTHOR_USERNAME};
 
 
 #[derive(Deserialize, Serialize)]
@@ -10,4 +13,54 @@ impl Default for Author {
     fn default() -> Self {
         Self { username: "DEFAULT".to_string() }
     }
+}
+
+
+pub fn get_all_authors(conn: &Connection) -> Result<Vec<Author>, rusqlite::Error> {
+    let mut stmt = conn.prepare(&format!("SELECT username FROM {}", AUTHOR_TABLE_NAME))?;
+
+    let author_iter = stmt.query_map([], |row| {
+        Ok(row.get(0)?)
+    })?;
+
+    let mut auths = Vec::new();
+
+    for username in author_iter {
+        match username {
+            Ok(nm) =>
+                auths.push(Author {
+                    username: nm
+                }),
+            Err(_) => continue, // TODO: Keep track of errors
+        }
+        
+    }
+
+    Ok(auths)
+}
+
+
+
+pub fn get_authors_by_username(conn: &Connection, username: String) -> Result<Vec<Author>, rusqlite::Error> {
+    let mut stmt = conn.prepare(&format!("SELECT username FROM {} WHERE username LIKE ?1", AUTHOR_TABLE_NAME))?;
+
+    // Places the username, where (?1) is, sanitizing the username
+    let author_iter = stmt.query_map([&username], |row| {
+        Ok(row.get(0)?)
+    })?;
+
+    let mut auths = Vec::new();
+
+    for username in author_iter {
+        match username {
+            Ok(nm) =>
+                auths.push(Author {
+                    username: nm
+                }),
+            Err(_) => continue, // TODO: Keep track of errors
+        }
+        
+    }
+
+    Ok(auths)
 }
--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,5 +1,10 @@
 pub mod db {
     pub const DB_NAME: &str = "gnf";
     pub const DB_PATH: &str = "gnf.db";
-    pub const ARTICLE_TABLE_NAME: &str = "article";
+    pub const ARTICLE_TABLE_NAME: &str = "articles";
+    
+    pub mod author {
+        pub const AUTHOR_TABLE_NAME: &str = "authors";
+        pub const AUTHOR_USERNAME: &str = "username";
+    }
 }
